f71f8792dbfba10d78dc2d664c2afa7179a7617a
Mo Albazaz <malbazaz@mba2018.hbs.edu>
2018-06-06 15:41:10 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,15 +1,9 @@
 // declare your functions here...
 
 function paragraphSelectop(){
-var 
 }
 
-  
-function paragraphSelector(){
-  
-for(var i=0; i<$('p'.count()); i++){
-  
-}
+
   
 function lastImageSelector(){
   return $('.img:last')
